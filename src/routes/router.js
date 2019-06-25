6efb461b075654a491cf382fcd3243cbc72ef0c4
Klypalskyi <klypalskyi@gmail.com>
2019-06-25 23:49:24 +0300

    Home work #2

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,13 @@
 const handleProductsRoute = require('./products/handle-products')
-const signUpRouter = require('./users/signUp');
-const mainRoute = require('./main/main');
+const signUpRouter = require('./users/signUp')
+const mainRoute = require('./main/main')
 debugger
 const router = {
     '/signup': signUpRouter,
     '/products': handleProductsRoute,
+    '/products/': handleProductsRoute,
     '/': mainRoute,
     default: mainRoute
-};
+}
 
 module.exports = router
