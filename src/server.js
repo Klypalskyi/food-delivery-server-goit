4e6297faa92c1c5e810784533f6d5f10720d096e
Klypalskyi <klypalskyi@gmail.com>
2019-07-11 00:31:07 +0300

    checked HW1

--- src/server.js
+++ src/server.js
@@ -1,21 +1,18 @@
 const http = require('http');
 const url = require('url');
-
 const morgan = require('morgan')
 const router = require('./routes/router');
-
 const logger = morgan('combined');
 
 const startServer = port => {
     const server = http.createServer(function(req, res) {
 
-        const urlParsed = url.parse(req.url);
-
+        const urlParsed = url.parse(req.url);   
         const func = router[urlParsed.pathname] || router.default;
 
         logger(req, res, () => func(req, res));
     });
-
+    
     server.listen(port);
 };
 
