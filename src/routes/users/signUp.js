016ba78d804b1f05b329345a05e8ad09da2c1b4c
Klypalskyi <klypalskyi@gmail.com>
2019-07-11 01:00:54 +0300

    add check query for signup

--- src/routes/users/signUp.js
+++ src/routes/users/signUp.js
@@ -12,29 +12,58 @@ const saveUser = user => {
     })
 }
 
+const checkUserFields = user => {
+    const userName = user.username;
+    const userPhone = user.telephone.replace(/\s/g, "");
+    const userPass = user.password;
+    const userEmail = user.email;
+    if (typeof userName === "string" &&
+        !isNaN(Number(userPhone)) &&
+        typeof userPass === "string" &&
+        typeof userEmail === "string")
+        return true
+    else return false
+    // console.log(typeof userName === "string");
+    // console.log(!isNaN(Number(userPhone)));
+    // console.log(typeof userPass === "string");
+    // console.log(typeof userEmail === "string");
+}
+
 const signUpRouter = (req, res) => {
     if (req.method === "POST") {
         let body = '';
-
         req
-        .on("data", function (data) {
-            body += data
+            .on("data", function (data) {
+                body += data
 
-        })
-        .on("end", function() {
-            const log = JSON.parse(body);
-            saveUser(log);
+            })
+            .on("end", function () {
+                const log = JSON.parse(body);
+                if (checkUserFields(log)) {
+                    saveUser(log);
+                    const finRes = {
+                        status: 'success',
+                        user: log
+                    }
 
-            const finRes = {
-                status: 'success',
-                user: log
-            }
+                    res.writeHead(200, {
+                        "Content-type": "application/json"
+                    });
+                    res.end(JSON.stringify(finRes));
+                } else {
+                    const finRes = {
+                        status: 'error',
+                        reason: 'False type at query'
+                    }
 
-            res.writeHead(200, {
-                "Content-type": "application/json"
+                    res.writeHead(403, {
+                        "Content-type": "application/json"
+                    });
+                    res.end(JSON.stringify(finRes));
+                }
+
+
             });
-            res.end(JSON.stringify(finRes));
-        });
     };
 };
 
