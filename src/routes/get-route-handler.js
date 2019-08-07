52374467dc93526131e00a7a9bcbcc46008dab11
Klypalskyi <klypalskyi@gmail.com>
2019-08-07 21:40:34 +0300

    hmw-2 branch

--- src/routes/get-route-handler.js
+++ src/routes/get-route-handler.js
@@ -2,10 +2,10 @@ const getClearUrl = url => {
     const lastIndex = url.lastIndexOf('/');
     const idString = url.slice(lastIndex + 1).trim();
     const idNumber = +idString
-
+    
     if (idNumber && lastIndex !== -1) {
         return url.slice(0, lastIndex);
-    } else if (lastIndex > 0){
+    } else if (lastIndex >= 0){
         return url.slice(0, lastIndex);
     }
 
--- src/server.js
+++ src/server.js
@@ -8,18 +8,31 @@ const router = require('./routes/router');
 const logger = morgan('combined');
 
 const keyPath = path.join(__dirname, "sert/key-20190213-131624.pem");
-const certPath =path.join(__dirname, "sert/cert-20190213-131624.crt")
+const certPath = path.join(__dirname, "sert/cert-20190213-131624.crt")
 
 const optionsSSL = {
     cert: fs.readFileSync(certPath),
     key: fs.readFileSync(keyPath)
 }
 
+const unHashUrl = url => {
+    const lastIndex = url.lastIndexOf('/');
+    if (lastIndex === 0) {
+        return url;
+    }
+
+    if (lastIndex !== -1) {
+        return url.slice(0, lastIndex)
+    }
+}
+
 const startServer = port => {
-    const server = https.createServer(optionsSSL, function(req, res) {
+    const server = https.createServer(optionsSSL, function (req, res) {
 
         const urlParsed = url.parse(req.url);
-        
+
+        unHashUrl(urlParsed);
+
         const func = router[urlParsed.pathname] || router.default
 
         logger(req, res, () => func(req, res));
